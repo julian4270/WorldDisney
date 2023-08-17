33afa4e0fd0a4d75b5b101a15b94401fd6017ca7
julian4270 <guitarrajulian@hotmail.com>
2023-08-17 12:00:28 -0500

    Add MYSQL Evidence
    Add MYSQL Evidence

---
+++ evidence/MySQL/Querys/disneydb_personaje.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: disneydb
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `personaje`
+--
+
+DROP TABLE IF EXISTS `personaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personaje` (
+  `Imagen` int NOT NULL,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Edad` varchar(45) DEFAULT NULL,
+  `Peso` varchar(45) DEFAULT NULL,
+  `Historia` varchar(45) DEFAULT NULL,
+  `Películas` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Imagen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personaje`
+--
+
+LOCK TABLES `personaje` WRITE;
+/*!40000 ALTER TABLE `personaje` DISABLE KEYS */;
+INSERT INTO `personaje` VALUES (1,'Donald','1','10','Animacion','El pato DOnald'),(2,'Mickey Mouse','5','20','Mickey Mouse','Mickey Mouse y sus amigos'),(3,'Ariel','4','15','Sirenita','La Sirenita'),(4,'Elsa','3','8','Frozen','Frozen');
+/*!40000 ALTER TABLE `personaje` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-17 11:58:45
